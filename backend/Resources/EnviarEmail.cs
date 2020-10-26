fa22d7b86754dbfdecc143e04570a82e2c784392
Sthefany <sthefanycristovam@gmail.com>
2020-10-26 14:08:04 -0300

    enviar email -- mudanças

--- backend/Resources/EnviarEmail.cs
+++ backend/Resources/EnviarEmail.cs
@@ -7,25 +7,13 @@ namespace backend.Resources
     public class EnviarEmail
     {
         
-        public void VerificarEmail(Models.Request.VerificarEmail req)
+        public void VerificarEmail(string destinatario)
         {
-
-            SmtpClient smtp = new SmtpClient();
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 587;
-            smtp.EnableSsl = true;
-            smtp.UseDefaultCredentials = false;
-            smtp.Credentials = new System.Net.NetworkCredential("gobookcompany@gmail.com", "gobook@123");
-
-            MailAddress from = new MailAddress("gobookcompany@gmail.com");
-            MailAddress to = new MailAddress(req.destinatario);
-
-
-            MailMessage message = new MailMessage(from, to);
-            message.Subject = "Verificação de Email";
-            message.Body = $"Olá! Você acabou de pedir a alteração da sua senha na GoBook. Seu código é: ${this.CriarCodigo()}. Obs: Caso você não tenha feito este pedido, ignore este e-mail.";            
-
-            smtp.Send(message);
+            this.EmailSimples(new Models.Request.VerificarEmail(){
+                destinatario = destinatario,
+                titulo = "Verificação de Email",
+                assunto = $"Olá! Você acabou de pedir a alteração da sua senha na GoBook. Seu código é: ${this.CriarCodigo()}. Obs: Caso você não tenha feito este pedido, ignore este e-mail."
+            });
         }
 
         public void EmailSimples(Models.Request.VerificarEmail req)
@@ -48,7 +36,7 @@ namespace backend.Resources
             smtp.Send(message);
         }
 
-        public string CriarCodigo()
+        private string CriarCodigo()
         {
             return null;
         }
