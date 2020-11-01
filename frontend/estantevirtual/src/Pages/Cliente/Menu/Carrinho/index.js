c3b0b691e96cb40ed05f3568c0a4079a403f6c6f
LucasLira-SP <lucas.palira@gmail.com>
2020-11-01 14:30:14 -0300

    frontend -- novas rotas

--- frontend/estantevirtual/src/Pages/Cliente/Menu/Carrinho/index.js
+++ frontend/estantevirtual/src/Pages/Cliente/Menu/Carrinho/index.js
@@ -14,6 +14,7 @@ function Carrinho(){
                 <div class="sidebarperfil">
                     <header>Menu goBook</header>
                     <ul>
+                        <li><Link to="/InicialCliente">Inicial</Link></li>
                         <li><Link to="/Perfil">Perfil</Link></li>
                         <li><Link to="/Estantes">Minhas estantes</Link></li>
                         <li><Link to="/Carrinho">Meu carrinho</Link></li>
--- frontend/estantevirtual/src/Pages/Cliente/Menu/Perfil/index.js
+++ frontend/estantevirtual/src/Pages/Cliente/Menu/Perfil/index.js
@@ -14,6 +14,7 @@ function Perfil (){
                 <div class="sidebarperfil">
                     <header>Menu goBook</header>
                     <ul>
+                        <li><Link to="/InicialCliente">Inicial</Link></li>
                         <li><Link to="/Perfil">Perfil</Link></li>
                         <li><Link to="/Estantes">Minhas estantes</Link></li>
                         <li><Link to="/Carrinho">Meu carrinho</Link></li>
