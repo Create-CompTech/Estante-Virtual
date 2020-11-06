ab77c06c66bcccd3469723edfb54822c5f4d9edf
patinhoszsz <gustavofurtado534@gmail.com>
2020-11-06 12:17:59 -0300

    frontend -- ajustes

--- frontend/estantevirtual/src/Pages/Cliente/index.js
+++ frontend/estantevirtual/src/Pages/Cliente/index.js
@@ -22,63 +22,10 @@ function Inicial(props) {
   }, []);
 
   return (
-    <div className="Inicial">
-      <header className="Inicial-Header">
-      
-        <div class="Logo"><h1>goBook</h1></div>
-        <div>Olá, {props.location.state.nome}!</div>
-
-        <div class="Pesquisa">
-          <input type="search" placeholder="Livro ou Autor" class="Texto"></input> 
-            <i class="fas fa-search"></i>
-        </div>
-      
-        <input type="checkbox" id="check"></input>
-          <label for="check">
-              <i class="fas fa-bars" id="btn"></i>
-              <i class="fas fa-bars" id="cancel"></i>
-          </label>
-          <div class="sidebar">
-              <header>Menu goBook</header>
-              <ul>
-                  <li><Link to="/Perfil">Perfil</Link></li>
-                  <li><Link to="/Estantes">Minhas estantes</Link></li>
-                  <li><Link to="/Carrinho">Meu carrinho</Link></li>
-              </ul>
-              <div class="botaologout">
-                <Link class="botaologout1" to="/">Logout</Link>
-                <i class="fas fa-sign-in-alt" id="btn1"></i>
-              </div>
-          </div>
-
-      </header>
-      <body>
-        <nav class="Cabeçalho">
-            <ul class="OpcoesCabeçalho">
-              <Link to=""><li class="OpcoesCabeçalho2">Livros e Autores</li></Link>
-              <Link to=""><li class="OpcoesCabeçalho2">Livros mais recomendados</li></Link>
-              <Link to=""><li class="OpcoesCabeçalho2">Livros mais vendidos</li></Link>
-            </ul>
-          </nav>
-      </body>
-
-
-        
-        <div className="titulo">Vários Ebooksss</div>
-
-        <div className="ebooks_box">
-          {ebooks.map( item => 
-            <div className="ebook">
-              <p>{item.nome}</p>
-              <p>{item.autor}</p>
-              <p>{item.sinopse}</p>
-            </div>
-          )}
-        </div>
-          
-        <div className="menu-baixo"></div>
-
-       </div>   
-    )};
+    <div className="InicialCliente">  
+    oi
+    </div>   
+    );
+}
 
     export default Inicial;
