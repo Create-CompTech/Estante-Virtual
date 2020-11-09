e7dfb38c6db7948d31f8685f8dd26e1ec279d874
patinhoszsz <gustavofurtado534@gmail.com>
2020-11-09 11:23:35 -0300

    frontend -- telas 3°entregae

--- frontend/estantevirtual/src/Pages/Cliente/index.js
+++ frontend/estantevirtual/src/Pages/Cliente/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import Header from '../../components/header';
 import { Link } from 'react-router-dom';
+import gobook_logo from '../../storage/images/logo/SizePinterest/gobook_logo.png';
+
 import './index.css';
-
-
 import ConsultarEbooks from '../../services/ConsultarEbooks';
+
 const api = new ConsultarEbooks();
 
 
@@ -25,9 +25,26 @@ function Inicial(props) {
   }, []);
 
   return (
-    <div className="InicialCliente">  
-      <Header /> 
-    </div>   
+    <div className="menu-principal">
+          <div className="MenuCadastro">
+            <input type="checkbox" id="check"></input>
+              <label for="check">
+                <i class="fas fa-bars" id="cancel"></i>
+              </label>
+        <div class="sidebarperfil">
+            <header>goBook</header>
+            <ul>
+                <li><Link to="/Perfil">Perfil</Link></li>
+                <li><Link to="/Estantes">Minhas estantes</Link></li>
+                <li><Link to="/Carrinho">Meu carrinho</Link></li>
+                <li><Link to="/Login">Sair</Link></li>
+                <div className="img">
+                <img src={gobook_logo} height="240px" width="255px"/>
+                </div>
+            </ul>
+        </div>
+    </div>
+    </div>
     );
 }
 
