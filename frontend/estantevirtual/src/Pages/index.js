dd4b8e117ef5add78b2dd052c63a726553a6b8d2
LucasLira-SP <lucas.palira@gmail.com>
2020-11-27 00:22:50 -0300

    frontend --ajustes tela inicial

--- frontend/estantevirtual/src/Pages/index.js
+++ frontend/estantevirtual/src/Pages/index.js
@@ -48,139 +48,317 @@ function Inicial() {
           </nav>
         </body>
         <body>
-          <div class="carousel-anuncios">
+        <div class="carousel-anuncios">
             <Carousel breakPoints={breakPoints1}>
               <ItemA>
                 <img src='/assets/anuncio.png' height="450px" width="900px"/>
               </ItemA>
-            </Carousel> 
-            <h1 class="titulo">Livros de Aventura</h1>
-              <div className="ebooks">
+            </Carousel>
+          </div> 
+
+            <div class="carousel-livros">
+              <h1 class="titulo">Livros de Aventura</h1>
+                <div className="App">
+                  <Carousel breakPoints={breakPoints}>
+                    <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                        <img src='/assets/HarryPotterPedraFilosofal' height="300px" width="200px"/>
+                        <div class="overlay">
+                          <a href="/harryPotterPedraFilosofal" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                    </Item>
+                    <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                      <img src='/assets/SenhorDosAneisTolkienV1' height="300px" width="200px"/>
+                      <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                    </Item>
+                    <Item>
+                      <div class="ebook">
+                        <div class="slide-img">
+                      <img src='/assets/PiratasDoCaribeJackSparrowOGuardiaoDoTempo.png' height="300px" width="200px"/>
+                      <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                    </Item>
+                    <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                      <img src='/assets/GravityFallsAventuraEmDobro.png' height="300px" width="200px"/>
+                      <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                    </Item>
+                    <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                      <img src='/assets/Eragon.png' height="300px" width="200px"/>
+                      <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                    </Item>
+                  </Carousel>
+                </div>
+            </div>
+            <div class="carousel-livros">
+              <h1 class="titulo">Livros de Romance</h1>
+                <div className="App">
                 <Carousel breakPoints={breakPoints}>
-                  <Item><a href="/HarryPotterPedraFilosofal"></a>
-                    <img src='/assets/HarryPotterPedraFilosofal' height="300px" width="200px"/>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/FilmeCulpaDasEstrelas.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="/ACulpaEDasEstrelas" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
                   </Item>
                   <Item>
-                    <img src='/assets/SenhorDosAneisTolkienV1' height="300px" width="200px"/>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AGarotaQueVoceDeixouParaTras.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
                   </Item>
                   <Item>
-                    <img src='/assets/PiratasDoCaribeJackSparrowOGuardiaoDoTempo.png' height="300px" width="200px"/>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/ACincoPassosDeVoce.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
                   </Item>
                   <Item>
-                    <img src='/assets/GravityFallsAventuraEmDobro.png' height="300px" width="200px"/>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AnnaEOBeijoFrances' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
                   </Item>
                   <Item>
-                    <img src='/assets/Eragon.png' height="300px" width="200px"/>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/UmQuartoComVista' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
                   </Item>
                 </Carousel>
+                </div>
+            </div>
+            <div class="carousel-livros">
+              <h1 class="titulo">Livros de Fantasia</h1>
+                <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AGuerraDosTronosAsCronicasDeGeloEFogo.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AsBrumasDeAvalonMarionZimmer.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/OLeaoAFeiticeiraEoGurdaRoupaCronicasDeNarnia.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/OMaravilhosoMagicoDeOz1900.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/OFeiticeiroDeTerramarUrsulaKLEGuin.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                </Carousel>
+                </div>
+            </div>
+
+            <div className="destaque">
+            <div className="container-destaque">
+              <div className="titulo-destaque">
+                <h1>Destaques da semana</h1>
               </div>
-          </div>
-          <div class="carousel-livros">
-            <h1 class="titulo">Livros de Romance</h1>
-              <div className="App">
-              <Carousel breakPoints={breakPoints}>
-                <Item><a href="/ACulpaEDasEstrelas"></a>
-                  <img src='/assets/FilmeCulpaDasEstrelas.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/AGarotaQueVoceDeixouParaTras.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/ACincoPassosDeVoce.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/AnnaEOBeijoFrances' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/UmQuartoComVista' height="300px" width="200px"/>
-                </Item>
-              </Carousel>
+              <div className="destaque-box">
+                <div className="livros-destaque">
+                <div className="destaque1"><img src='/assets/OQueAconteceuComAnnie.png' height="220px" width="200px" ></img></div> 
+                <div className="destaque1"><img src='/assets/AGarotaDoLago.png' height="220px" width="200px" ></img></div> 
+                <div className="destaque1"><img src='/assets/OHobbitJJRTolkien.png' height="220px" width="200px" ></img></div> 
+                <div className="destaque1"><img src='/assets/AsCronicasDeNarnia2009.png' height="220px" width="200px" ></img></div> 
+                <div className="destaque1"><img src='/assets/ZaynMalik.png' height="220px" width="200px" ></img></div> 
+                </div>
               </div>
-          </div>
-          <div class="carousel-livros">
-            <h1 class="titulo">Livros de Fantasia</h1>
-              <div className="App">
-              <Carousel breakPoints={breakPoints}>
-                <Item>
-                  <img src='/assets/AGuerraDosTronosAsCronicasDeGeloEFogo.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/AsBrumasDeAvalonMarionZimmer.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/OLeaoAFeiticeiraEoGurdaRoupaCronicasDeNarnia.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/OMaravilhosoMagicoDeOz1900.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/OFeiticeiroDeTerramarUrsulaKLEGuin.png' height="300px" width="200px"/>
-                </Item>
-              </Carousel>
-              </div>
+            </div>
           </div>
 
-          <div className="destaque">
-          <div className="container-destaque">
-            <div className="titulo-destaque">
-              <h1>Destaques da semana</h1>
+            <div class="carousel-livros">
+              <h1 class="titulo">Livros de Terror</h1>
+                <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/ItACoisaStephenKing.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/OCemiterioStephenKing.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AAssombracaoDaCasaDaColina.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/QuatroEstacoesStephenKing.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/GuerraMundialZ.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                </Carousel>
+                </div>
             </div>
-            <div className="destaque-box">
-              <div className="livros-destaque">
-               <div className="destaque1"><img src='/assets/OQueAconteceuComAnnie.png' height="220px" width="200px" ></img></div> 
-               <div className="destaque1"><img src='/assets/AGarotaDoLago.png' height="220px" width="200px" ></img></div> 
-               <div className="destaque1"><img src='/assets/OHobbitJJRTolkien.png' height="220px" width="200px" ></img></div> 
-               <div className="destaque1"><img src='/assets/AsCronicasDeNarnia2009.png' height="220px" width="200px" ></img></div> 
-               <div className="destaque1"><img src='/assets/ZaynMalik.png' height="220px" width="200px" ></img></div> 
-              </div>
+            <div class="carousel-livros">
+              <h1 class="titulo">Livros de Zumbis</h1>
+                <div className="App">
+                <Carousel breakPoints={breakPoints}>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                      <img src='/assets/TheWalkingDeadV1.png' height="300px" width="200px"/>
+                      <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/TerioEPraticaDeComoSerUmZumbiTiagoToy.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/AIlhaDosMortosRodrigoDeOliveira.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                  <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/StrangerThingsGarotosZumbis.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                  <Item>
+                    <div class="ebook">
+                      <div class="slide-img">
+                    <img src='/assets/ApocalipseZumbi3AlexandreCallari.png' height="300px" width="200px"/>
+                    <div class="overlay">
+                          <a href="#" class="vermais-btn">Ver mais</a>	
+                        </div>
+                      </div>
+                    </div>
+                  </Item>
+                </Carousel>
+                </div>
             </div>
-          </div>
-        </div>
-        
-          <div class="carousel-livros">
-            <h1 class="titulo">Livros de Terror</h1>
-              <div className="App">
-              <Carousel breakPoints={breakPoints}>
-                <Item>
-                  <img src='/assets/ItACoisaStephenKing.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/OCemiterioStephenKing.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/AAssombracaoDaCasaDaColina.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/QuatroEstacoesStephenKing.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/GuerraMundialZ.png' height="300px" width="200px"/>
-                </Item>
-              </Carousel>
-              </div>
-          </div>
-          <div class="carousel-livros">
-            <h1 class="titulo">Livros de Terror</h1>
-              <div className="App">
-              <Carousel breakPoints={breakPoints}>
-                <Item>
-                  <img src='/assets/TheWalkingDeadV1.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/TerioEPraticaDeComoSerUmZumbiTiagoToy.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/AIlhaDosMortosRodrigoDeOliveira.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/StrangerThingsGarotosZumbis.png' height="300px" width="200px"/>
-                </Item>
-                <Item>
-                  <img src='/assets/ApocalipseZumbi3AlexandreCallari.png' height="300px" width="200px"/>
-                </Item>
-              </Carousel>
-              </div>
-          </div>
           
         </body>
 
